24321b8a16f2ea55084e604625c181c8814783e3
REDIVIOUS <wlding@hust.edu.cn>
2019-10-14 20:30:21 +0800

    v0.0.5 R comments on study

--- src/pages/study/index.vue
+++ src/pages/study/index.vue
@@ -1,17 +1,21 @@
 <template>
   <div>
+    <!-- 难度选择页面结构 -->
     <div class="picker-area">
       <picker @change="bindPickerChange" :value="index" :range="array">
         <view class="picker">{{array[index]}} ▼</view>
       </picker>
     </div>
+    <!-- 简单难度页面结构 -->
     <div class="card-area easy" v-show="easyDisplay">
+      <!-- 10以内加法内容 -->
       <div class="card add10" @click="gotoClass">
         <p>十以内加法</p>
         <p>知识点数:20</p>
         <p>学习单元数:30</p>
         <p>学习进度</p>
       </div>
+      <!-- 10以内减法内容 -->
       <div class="card sub10">
         <p>十以内减法</p>
         <p>知识点数:50</p>
@@ -29,6 +33,7 @@
         <p></p>
       </div> -->
     </div>
+    <!-- 中等难度页面结构 -->
     <div class="card-area medium" v-show="mediumDisplay">
       <!-- <div class="card mul10">
         <p></p>
@@ -56,6 +61,7 @@
         <p></p>
       </div> -->
     </div>
+    <!-- 困难难度页面结构 -->
     <div class="card-area hard" v-show="hardDisplay">
       <!-- <div class="card add10">
         <p></p>
@@ -118,29 +124,32 @@
 
 <script>
 export default {
+  // 学习选择界面显示数据
   data () {
     return {
       index: 0,
-      array: ['简单', '中等', '困难'],
-      easyDisplay: true,
-      mediumDisplay: false,
-      hardDisplay: false
+      array: ['简单', '中等', '困难'], // 难度选择列表
+      easyDisplay: true, //选择简单难度
+      mediumDisplay: false, // 选择中等难度
+      hardDisplay: false //选择困难难度
       // urls: []       // 链接
       // subjects: []   // 项目
     }
   },
+  // 页面控制函数列表
   methods: {
+    // 选择难度的控制函数
     bindPickerChange (e) {
       this.index = e.target.value
-      if (this.index === '0') {
+      if (this.index === '0') { // 简单难度控制函数
         this.easyDisplay = true
         this.mediumDisplay = false
         this.hardDisplay = false
-      } else if (this.index === '1') {
+      } else if (this.index === '1') { // 中等难度控制函数
         this.easyDisplay = false
         this.mediumDisplay = true
         this.hardDisplay = false
-      } else {
+      } else { // 困难难度控制函数
         this.easyDisplay = false
         this.mediumDisplay = false
         this.hardDisplay = true
@@ -148,6 +157,7 @@ export default {
       console.log(this.easyDisplay)
       console.log(this.index === '1')
     },
+    // 跳转到学习页面控制函数
     gotoClass () {
       mpvue.navigateTo({url: '/pages/class/main'})
     }
